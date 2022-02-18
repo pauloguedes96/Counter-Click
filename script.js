e4d9a8d2ae4bcb1a1ac6973b6308d0965b1c5250
Paulo Guedes <cesarmota94@gmail.com>
2022-02-17 23:35:19 -0300

    c3

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ let number = document.querySelector('#number');
 let counterStorage = null;
 
 let changeThemeClicked = false;
+let themeLigthMode = false;
+
 let btnChange = document.querySelector('#changeTheme');
 let background = document.querySelector('body');
 let btnReset = document.querySelector('#btnReset');
@@ -12,6 +14,8 @@ let title = document.querySelector('#title');
 let footer = document.querySelector('footer');
 let footerImg = document.querySelector('footer img');
 
+let dadosStorage = {}
+let dadosTemp = null;
 
 function incremento() {
     counter++;
@@ -54,45 +58,70 @@ function numberColor() {
 }
 
 function persistenciaGrava() {
-    localStorage.setItem('counter', JSON.stringify(counter));
+
+    dadosStorage = {
+        counter: counter,
+        themeLigthMode: themeLigthMode,
+    }
+
+    localStorage.setItem('dados', JSON.stringify(dadosStorage));
 }
 
 function recuperaStorage() {
-    counter = Number(localStorage.getItem('counter'));
-    numberColor();
-    number.innerHTML = counter;
+
+    dadosTemp = JSON.parse(localStorage.getItem('dados'));
+    if (dadosTemp) {
+        counter = dadosTemp.counter;
+        numberColor();
+        number.innerHTML = dadosTemp.counter;
+        console.log(dadosTemp)
+
+        if(dadosTemp.themeLigthMode){
+            themeLight();
+        }
+    }
 }
 
 function changeTheme() {
 
     if (!changeThemeClicked) {
-
-        btnChange.style.justifyContent = 'flex-end'
-        background.style.backgroundColor = 'white';
-        btnReset.style.backgroundColor = 'rgb(85, 85, 85)';
-        btnReset.style.borderColor = 'rgb(26, 26, 26)';
-        btnReset.style.color = 'white';
-        title.style.color = 'black';
-        number.style.color = 'black';
-        footer.style.color = 'black';
-        footerImg.src = './icons/GitHub-Mark-32px.png'
-
-        changeThemeClicked = true;
-        numberColor()
+        themeLight();
 
     } else {
-        btnChange.style.justifyContent = 'flex-start'
-        background.style.backgroundColor = 'rgb(17, 17, 17)';
-        btnReset.style.backgroundColor = '#EFEFEF';
-        btnReset.style.borderColor = '#555555';
-        btnReset.style.color = 'black';
-        title.style.color = 'white';
-        number.style.color = 'white';
-        footer.style.color = 'white';
-        footerImg.src = './icons/GitHub-Mark-Light-32px.png'
-
-
-        changeThemeClicked = false;
-        numberColor()
+        themeDark();
     }
+}
+
+function themeLight() {
+    btnChange.style.justifyContent = 'flex-end'
+    background.style.backgroundColor = 'white';
+    btnReset.style.backgroundColor = 'rgb(85, 85, 85)';
+    btnReset.style.borderColor = 'rgb(26, 26, 26)';
+    btnReset.style.color = 'white';
+    title.style.color = 'black';
+    number.style.color = 'black';
+    footer.style.color = 'black';
+    footerImg.src = './icons/GitHub-Mark-32px.png';
+
+    changeThemeClicked = true;
+    themeLigthMode = true;
+    numberColor()
+    persistenciaGrava()
+}
+
+function themeDark() {
+    btnChange.style.justifyContent = 'flex-start'
+    background.style.backgroundColor = 'rgb(17, 17, 17)';
+    btnReset.style.backgroundColor = '#EFEFEF';
+    btnReset.style.borderColor = '#555555';
+    btnReset.style.color = 'black';
+    title.style.color = 'white';
+    number.style.color = 'white';
+    footer.style.color = 'white';
+    footerImg.src = './icons/GitHub-Mark-Light-32px.png';
+
+    changeThemeClicked = false;
+    themeLigthMode = false;
+    numberColor()
+    persistenciaGrava()
 }
